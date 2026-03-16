f613d17b3cd575de580537f0b0a29c0858301602
George Gvazava <ggvaz2020@agruni.edu.ge>
2026-03-16 21:55:27 +0400

    Compressed the VHS tape

--- files.js
+++ files.js
@@ -7,42 +7,6 @@ export const projectData = [
         shortDesc: "test_01",
         url: "filepage/project_01.html" // Points to your specific file
     },
-    {
-        title: "Project_02",
-        category: "Category_02",    
-        shortDesc: "test_02",
-        url: "filepage/project_02.html" // Points to your specific file
-    },
-    {
-        title: "Project_03",
-        category: "Category_03",
-        shortDesc: "test_03",
-        url: "filepage/project_03.html" // Points to your specific file
-    },
-    {
-        title: "Project_04",
-        category: "Category_04",
-        shortDesc: "test_04",
-        url: "filepage/project_04.html" // Points to your specific file
-    },
-    {
-        title: "Project_05",
-        category: "Category_05",
-        shortDesc: "test_05",
-        url: "filepage/project_05.html" // Points to your specific file
-    },
-        {
-        title: "Project_04",
-        category: "Category_04",
-        shortDesc: "test_04",
-        url: "filepage/project_04.html" // Points to your specific file
-    },
-    {
-        title: "Project_05",
-        category: "Category_05",
-        shortDesc: "test_05",
-        url: "filepage/project_05.html" // Points to your specific file
-    },
 
 
 ];
